56754079146cfd0d6fbf09eded74378c233a30d4
LRY1994 <949892801@qq.com>
2019-06-11 11:54:32 +0800

    钟智写的

--- Config.cpp
+++ Config.cpp
@@ -1,16 +1,74 @@
-double cal_power(double a, double b)
-{
-    return 0;
+// battery model section
+// all parameters are here
+double Tenv = -10;
+double E = 0;
+double I = 1;
+double deltax = 0.1;
+double lamda = 0.1;
+double ha = 0.1;
+double A = 1;
+double R = 1;
+double Pmax = 500;
+double Qt = 30;
+double Q0 = 37;
+double Ctotal = 100;
+double time = 1;
+// double Pcool;
+// double Pexo; 
+// double Pptc;
+// double deltaT;
+
+//散热功率
+double getPcool(double T,double Tnex) {
+    return ((Tnex+T)/2-Tenv)/(deltax/(lamda*A)+1/(ha*A));
 }
-double get_hightest_temp(double origin)
-{
-    return origin + 1;
+//发热功率
+double getPexo() {
+    return I*I*R;
 }
-double get_lowest_temp(double origin)
-{
-    return origin - 1;
+//温度变化量
+double getdeltaT(double Pptc,double Pcool,double Pexo) {
+    return (Pptc-Pcool+Pexo)*time/Ctotal;
+}
+//加热功率
+double getPptc(double T,double Tnex,double Pcool,double Pexo) { 
+    return (Tnex-T)*Ctotal/time+Pcool-Pexo;
 }
 
-double get_firstLayer_temp(int index,double rootVal){
-    return index * rootVal;
+
+double get_highest_temp(double T)
+{  
+    double Pcool = getPcool(T,T);
+    double Pexo = getPexo();
+    double Pptc = Pmax;
+    return getdeltaT(Pptc,Pcool,Pexo) + T;
+
+}
+
+double get_lowest_temp(double T)
+{
+    double Pcool = getPcool(T,T);
+    double Pexo = getPexo();
+    double Pptc = 0;
+    return getdeltaT(Pptc,Pcool,Pexo) + T;
+}
+
+double get_firstLayer_temp(int n,double parent,int N){
+    double Tmin = get_lowest_temp(parent);
+    double Tmax = get_highest_temp(parent);
+
+    return (2*n+1)/2*N*Tmin+(2*N-2*n-1)/2*N*Tmax;
+   
+}
+
+double cal_power(double parent, double child)
+{
+    double Tmin = get_lowest_temp(parent);
+    double Tmax = get_highest_temp(parent);
+    double Pcool = getPcool(parent,child);
+    double Pexo = getPexo();
+    double Pptc = getPptc(parent,child,Pcool,Pexo);
+       
+    return (Pptc/48+I)*Qt/Q0*time;
+
 }
